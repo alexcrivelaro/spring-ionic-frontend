67d3c69e0b417b1e932da950d2c72a3f23f372e8
Alexandre Crivelaro <alexcrivelaro@hotmail.com>
2018-05-31 14:37:34 -0300

    Criando pagina de produtos

--- src/pages/categorias/categorias.ts
+++ src/pages/categorias/categorias.ts
@@ -31,8 +31,12 @@ export class CategoriasPage {
   ionViewDidLoad() {
     this.categoriaService.findAll()
       .subscribe(response => {
-       this.items = response;
+        this.items = response;
       },
-    error => {});
+      error => {});
+  }
+
+  showProdutos() {
+    this.navCtrl.push('ProdutosPage');    
   }
 }
